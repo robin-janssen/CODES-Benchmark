ad81e637d43eed9473c8e7f721d95d1c12b9def1
Robin <robin.janssen@stud.uni-heidelberg.de>
2024-06-20 13:20:12 +0200

    Updated AbstractSurrogateModel class

--- surrogates/surrogate_classes.py
+++ surrogates/surrogate_classes.py
@@ -1,9 +1,11 @@
 from surrogates.DeepONet.deeponet import MultiONet
 from surrogates.FCNN.fcnn import FullyConnected
+from surrogates.NeuralODE.neural_ode import NeuralODE
 
 # Define surrogate classes
 surrogate_classes = {
     "DeepONet": MultiONet,
     "FCNN": FullyConnected,
+    "NeuralODE": NeuralODE,
     # Add any additional surrogate classes here
 }
--- surrogates/surrogates.py
+++ surrogates/surrogates.py
@@ -1,8 +1,10 @@
 from abc import ABC, abstractmethod
-from typing import Optional, Union
 
-from torch import nn
-from torch import Tensor
+# from typing import Optional, Union
+
+from torch import nn, Tensor
+from torch.utils.data import DataLoader
+import numpy as np
 
 
 # Define abstract base class for surrogate models
@@ -12,23 +14,44 @@ class AbstractSurrogateModel(ABC, nn.Module):
         super().__init__()
 
     @abstractmethod
-    def forward(self, x):
+    def forward(self, inputs: tuple[Tensor, Tensor, Tensor]) -> Tensor:
         pass
 
     @abstractmethod
-    def prepare_data(self, data_loader):
+    def prepare_data(
+        self,
+        dataset: np.ndarray,
+        timesteps: np.ndarray,
+        batch_size: int | None,
+        shuffle: bool,
+    ):
         pass
 
     @abstractmethod
-    def fit(self, conf, data_loader, test_loader=None):
+    def fit(
+        self,
+        train_loader: DataLoader | Tensor,
+        test_loader: DataLoader | Tensor,
+        timesteps: np.ndarray,
+        epochs: int | None,
+    ) -> None:
         pass
 
     @abstractmethod
-    def predict(self, data_loader):
+    def predict(
+        self,
+        data_loader: DataLoader | Tensor,
+        criterion: nn.Module,
+        timesteps: np.ndarray,
+    ) -> tuple[float, np.ndarray, np.ndarray]:
         pass
 
     @abstractmethod
     def save(
-        self, model_name, config, subfolder, train_loss, test_loss, training_duration
-    ):
+        self,
+        model_name: str,
+        subfolder: str,
+        training_id: str,
+        dataset_name: str,
+    ) -> None:
         pass
