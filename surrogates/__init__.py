26c23ae4ae9897b253b8467bcfa9b5a2d9551e87
Robin <robin.janssen@stud.uni-heidelberg.de>
2024-08-08 16:44:34 +0200

    new run

--- surrogates/__init__.py
+++ surrogates/__init__.py
@@ -1,6 +1,4 @@
 from .surrogate_classes import surrogate_classes
 from .surrogates import AbstractSurrogateModel
-from .DeepONet.deeponet import MultiONet
-from .LatentPolynomial.latent_poly import LatentPoly
 
-__all__ = ["surrogate_classes", "AbstractSurrogateModel", "MultiONet", "LatentPoly"]
+__all__ = ["surrogate_classes", "AbstractSurrogateModel"]
