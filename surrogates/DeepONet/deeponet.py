fix normalisation in MultiONet and FCNN

surrogates/DeepONet/deeponet.py
@@ -342,42 +342,19 @@ class MultiONet(OperatorNetwork):
         timesteps,
         batch_size=32,
         shuffle: bool = False,
-        normalize: bool = True,
-        fraction: float = 1,
-        train: bool = False,
     ):
-        """
-        Create a DataLoader with optional fractional subsampling for chemical evolution data for DeepONet.
-
-        :param data: 3D numpy array with shape (num_samples, len(timesteps), num_chemicals)
-        :param timesteps: 1D numpy array of timesteps.
-        :param fraction: Fraction of the grid points to sample.
-        :param batch_size: Batch size for the DataLoader.
-        :param shuffle: Whether to shuffle the data.
-        :param normalize: Whether to normalize the data.#
-        :param device: Device to use.
-        :param train: Whether the provided data is training data. If so, the mean and standard deviation are computed.
-        :return: A DataLoader object.
-        """
+        """ """
         # Initialize lists to store the inputs and targets
         branch_inputs = []
         trunk_inputs = []
         targets = []
 
         # Iterate through the grid to select the samples
-        if fraction == 1:
-            for i in range(data.shape[0]):
-                for j in range(data.shape[1]):
-                    branch_inputs.append(data[i, 0, :])
-                    trunk_inputs.append([timesteps[j]])
-                    targets.append(data[i, j, :])
-        else:
-            for i in range(data.shape[0]):
-                for j in range(data.shape[1]):
-                    if np.random.uniform(0, 1) < fraction:
-                        branch_inputs.append(data[i, :, 0])
-                        trunk_inputs.append([timesteps[j]])
-                        targets.append(data[i, :, j])
+        for i in range(data.shape[0]):
+            for j in range(data.shape[1]):
+                branch_inputs.append(data[i, 0, :])
+                trunk_inputs.append([timesteps[j]])
+                targets.append(data[i, j, :])
 
         # Convert to PyTorch tensors
         branch_inputs_tensor = torch.tensor(
@@ -385,16 +362,6 @@ class MultiONet(OperatorNetwork):
         )
         trunk_inputs_tensor = torch.tensor(np.array(trunk_inputs), dtype=torch.float32)
         targets_tensor = torch.tensor(np.array(targets), dtype=torch.float32)
-
-        if train:
-            self.b_mean = branch_inputs_tensor.mean()
-            self.b_std = branch_inputs_tensor.std()
-            self.target_mean = targets_tensor.mean()
-            self.target_std = targets_tensor.std()
-
-        if normalize:
-            branch_inputs_tensor = (branch_inputs_tensor - self.b_mean) / self.b_std
-            targets_tensor = (targets_tensor - self.target_mean) / self.target_std
 
         # Create a TensorDataset and DataLoader
         branch_inputs_tensor = branch_inputs_tensor.to(self.device)