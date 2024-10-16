8f95c246791f480fa86482d80d5f032a26f9d595
Robin <robinj0503@gmail.com>
2024-10-16 20:03:07 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,17 +1,12 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "CODES"
 copyright = "2024, Robin Janssen, Immanuel Sulzer"
 author = "Robin Janssen, Immanuel Sulzer"
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
     "sphinx.ext.autodoc",
@@ -27,29 +22,15 @@ autosummary_output_dir = "_autosummary"
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_book_theme"
 
 html_static_path = ["_static"]
 html_favicon = "_static/favicon-96x96.png"
+
+# Adjusted HTML theme options
 html_theme_options = {
-    "external_links": [
-        {
-            "name": "CODES GitHub",
-            "url": "https://github.com/robin-janssen/CODES-Benchmark",
-            "icon": "fa-brands fa-square-github",
-            "type": "fontawesome",
-        },
-        {
-            "name": "CODES Docs",
-            "url": "../../index.html",
-            "icon": "favicon-96x96.png",
-            "type": "local",
-        },
-    ],
     "icon_links": [
         {
             "name": "CODES GitHub",
@@ -60,9 +41,21 @@ html_theme_options = {
         {
             "name": "CODES Docs",
             "url": "../../index.html",
-            "icon": "_static/favicon-96x96.png",
+            "icon": "_static/favicon-96x96.png",  # Make sure this is the correct path to the favicon
             "type": "local",
         },
+        # {
+        #     "name": "Exemplary Badge",
+        #     "url": "https://img.shields.io/badge/Exemplary-Yes-brightgreen",
+        #     "icon": "https://img.shields.io/badge/Exemplary-Yes-brightgreen",
+        #     "type": "url",
+        # },
+        # { # Link to the CODES paper once its on arxiv!
+        #     "name": "CODES paper",
+        #     "url": "https://arxiv.org/abs/2106.04420",
+        #     "icon": "fa-file-pdf",
+        #     "type": "fontawesome",
+        # }
     ],
     "icon_links_label": "Quick Links",
 }
