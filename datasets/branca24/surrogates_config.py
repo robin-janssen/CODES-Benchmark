75b4bee6491cfdf49eddf5b5e5c03633ae9f12c8
Robin <robin.janssen@stud.uni-heidelberg.de>
2024-10-18 17:58:06 +0200

    Add optimal params for branca24

--- datasets/branca24/surrogates_config.py
+++ datasets/branca24/surrogates_config.py
@@ -7,42 +7,42 @@ from torch import nn
 class MultiONetConfig:
     """Model config for MultiONet for the osu2008 dataset"""
 
-    branch_hidden_layers: int = 4
-    trunk_hidden_layers: int = 7
-    hidden_size: int = 150
-    output_factor: int = 40
-    learning_rate: float = 0.0005
-    activation: nn.Module = nn.LeakyReLU()
+    branch_hidden_layers: int = 8
+    trunk_hidden_layers: int = 8
+    hidden_size: int = 280
+    output_factor: int = 30
+    learning_rate: float = 3e-4
+    activation: nn.Module = nn.GELU()
 
 
 @dataclass
 class LatentNeuralODEConfig:
     """Model config for LatentNeuralODE for the osu2008 dataset"""
 
-    latent_features: int = 10
+    latent_features: int = 7
     layers_factor: int = 46
-    learning_rate: float = 0.005
-    ode_activation: nn.Module = nn.Softplus()
-    ode_tanh_reg: bool = False
-    coder_activation: nn.Module = nn.ReLU()
+    learning_rate: float = 0.003
+    ode_activation: nn.Module = nn.Tanh()
+    ode_tanh_reg: bool = True
+    coder_activation: nn.Module = nn.Tanh()
 
 
 @dataclass
 class FullyConnectedConfig:
     """Model config for FullyConnected for the osu2008 dataset"""
 
-    hidden_size: int = 400
+    hidden_size: int = 80
     num_hidden_layers: int = 2
     learning_rate: float = 1.5e-5
-    activation: nn.Module = nn.Tanh()
+    activation: nn.Module = nn.LeakyReLU()
 
 
 @dataclass
 class LatentPolyConfig:
     """Model config for LatentPoly for the osu2008 dataset"""
 
-    latent_features: int = 5
-    degree: int = 6
-    learning_rate: float = 0.002
+    latent_features: int = 10
+    degree: int = 1
+    learning_rate: float = 5e-4
     layers_factor: int = 50
-    coder_activation: nn.Module = nn.ReLU()
+    coder_activation: nn.Module = nn.GELU()
