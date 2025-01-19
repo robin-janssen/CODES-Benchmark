8c2319025728ef35bdccd3107bf84da0414ad39a
Robin <robin.janssen@stud.uni-heidelberg.de>
2025-01-19 19:52:35 +0100

    Attempts at fixing non-determinism in multithreading

--- run_training.py
+++ run_training.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 from datetime import timedelta
 
+import torch
 from tqdm import tqdm
 
 from codes.train import (
@@ -28,6 +29,7 @@ def main(args):
     Args:sc
         args (Namespace): The command line arguments.
     """
+    torch.use_deterministic_algorithms(True)
     config = load_and_save_config(config_path=args.config, save=False)
     download_data(config["dataset"]["name"])
     task_list_filepath, copy_config = check_training_status(config)
