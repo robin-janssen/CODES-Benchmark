4605660bdd838379b19f5471170be1013ab5242f
Robin <robin.janssen@stud.uni-heidelberg.de>
2025-02-05 18:04:36 +0100

    First settings for blm

---
+++ datasets/branca_large_myr/surrogates_config.py
@@ -0,0 +1,49 @@
+from dataclasses import dataclass
+
+from torch import nn
+
+
+@dataclass
+class MultiONetConfig:
+    """Model config for MultiONet for the osu2008 dataset"""
+
+    branch_hidden_layers: int = 5
+    trunk_hidden_layers: int = 10
+    hidden_size: int = 1000
+    output_factor: int = 50
+    learning_rate: float = 0.0001  # 0.0001
+    activation: nn.Module = nn.LeakyReLU()
+
+
+@dataclass
+class LatentNeuralODEConfig:
+    """Model config for LatentNeuralODE for the osu2008 dataset"""
+
+    latent_features: int = 9
+    layers_factor: int = 86
+    learning_rate: float = 0.0007
+    ode_hidden: int = 3
+    ode_layer_width: int = 412
+    ode_tanh_reg: bool = True
+    activation: nn.Module = nn.ReLU()
+
+
+@dataclass
+class FullyConnectedConfig:
+    """Model config for FullyConnected for the osu2008 dataset"""
+
+    hidden_size: int = 1000
+    num_hidden_layers: int = 6
+    learning_rate: float = 0.00005
+    activation: nn.Module = nn.Tanh()
+
+
+@dataclass
+class LatentPolyConfig:
+    """Model config for LatentPoly for the osu2008 dataset"""
+
+    latent_features: int = 8
+    degree: int = 7
+    learning_rate: float = 0.0004  # 0.001
+    layers_factor: int = 84
+    activation: nn.Module = nn.ReLU()
