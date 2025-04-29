6d68f1dcf12848539720483ad2f547a104571dc8
Robin <robin.janssen@stud.uni-heidelberg.de>
2025-04-29 11:10:35 +0200

    update optimal hyperparams for primordial

--- datasets/primordial/surrogates_config.py
+++ datasets/primordial/surrogates_config.py
@@ -8,10 +8,10 @@ class MultiONetConfig:
     """Model config for MultiONet for the simple_ode dataset"""
 
     branch_hidden_layers: int = 8
-    trunk_hidden_layers: int = 6
-    hidden_size: int = 250
-    output_factor: int = 115
-    learning_rate: float = 1e-4
+    trunk_hidden_layers: int = 8
+    hidden_size: int = 130
+    output_factor: int = 98
+    learning_rate: float = 2e-4
     activation: nn.Module = nn.Tanh()
 
 
@@ -19,14 +19,14 @@ class MultiONetConfig:
 class LatentNeuralODEConfig:
     """Model config for LatentNeuralODE for the simple_ode dataset"""
 
-    latent_features: int = 9
+    latent_features: int = 10
     coder_layers: int = 4
-    coder_width: int = 230
-    learning_rate: float = 0.0004
-    ode_layers: int = 5
-    ode_width: int = 150
+    coder_width: int = 350
+    learning_rate: float = 2e-4
+    ode_layers: int = 10
+    ode_width: int = 157
     ode_tanh_reg: bool = True
-    activation: nn.Module = nn.LeakyReLU()
+    activation: nn.Module = nn.Softplus()
     model_version: str = "v2"
 
 
@@ -34,10 +34,10 @@ class LatentNeuralODEConfig:
 class FullyConnectedConfig:
     """Model config for FullyConnected for the simple_ode dataset"""
 
-    hidden_size: int = 800
-    num_hidden_layers: int = 8
-    learning_rate: float = 3e-5
-    activation: nn.Module = nn.GELU()
+    hidden_size: int = 453
+    num_hidden_layers: int = 1
+    learning_rate: float = 5e-4
+    activation: nn.Module = nn.ReLU()
 
 
 @dataclass
@@ -46,7 +46,7 @@ class LatentPolyConfig:
 
     latent_features: int = 9
     degree: int = 1
-    learning_rate: float = 0.0002
+    learning_rate: float = 2e-4
     coder_layers: int = 2
     coder_width: int = 264
     activation: nn.Module = nn.LeakyReLU()
