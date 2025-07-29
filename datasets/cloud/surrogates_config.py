56cb6d9d0ef31a756e9219cece023a713a2a0f66
Robin <robin.janssen@stud.uni-heidelberg.de>
2025-07-29 15:04:29 +0200

    unrelated tuning improvements/results

--- datasets/cloud/surrogates_config.py
+++ datasets/cloud/surrogates_config.py
@@ -7,50 +7,87 @@ from torch import nn
 class MultiONetConfig:
     """Model config for MultiONet for the simple_ode dataset"""
 
-    # cloud, trial 69
-    branch_hidden_layers: int = 1
-    trunk_hidden_layers: int = 9
-    hidden_size: int = 225
-    output_factor: int = 63
-    learning_rate: float = 4e-5  # optimal for ~4000 epochs
-    activation: nn.Module = nn.Tanh()
+    loss_function: nn.Module = nn.SmoothL1Loss()
+    optimizer: str = "adamw"
+    scheduler: str = "schedulefree"
 
 
 @dataclass
 class LatentNeuralODEConfig:
     """Model config for LatentNeuralODE for the simple_ode dataset"""
 
-    # cloud, trial 63
-    latent_features: int = 6
-    coder_layers: int = 2
-    coder_width: int = 103
-    learning_rate: float = 3e-4
-    ode_layers: int = 4
-    ode_width: int = 197
-    ode_tanh_reg: bool = True
-    activation: nn.Module = nn.SiLU()
-    model_version: str = "v2"
+    loss_function: nn.Module = nn.MSELoss()
+    optimizer: str = "adamw"
+    scheduler: str = "schedulefree"
+    ode_tanh_reg: bool = False
 
 
 @dataclass
 class FullyConnectedConfig:
     """Model config for FullyConnected for the simple_ode dataset"""
 
-    # cloud, trial 44
-    hidden_size: int = 261
-    num_hidden_layers: int = 1
-    learning_rate: float = 1e-4
-    activation: nn.Module = nn.LeakyReLU()
+    loss_function: nn.Module = nn.SmoothL1Loss()
+    optimizer: str = "adamw"
+    scheduler: str = "schedulefree"
 
 
 @dataclass
 class LatentPolyConfig:
     """Model config for LatentPoly for the simple_ode dataset"""
 
-    # cloud, trial 92
-    latent_features: int = 9
-    degree: int = 5
-    learning_rate: float = 3e-4
-    coder_layers: int = 1
-    coder_width: int = 86
-    activation: nn.Module = nn.Mish()
+    loss_function: nn.Module = nn.MSELoss()
+    optimizer: str = "adamw"
+    scheduler: str = "schedulefree"
+
+
+# @dataclass
+# class MultiONetConfig:
+#     """Model config for MultiONet for the simple_ode dataset"""
+
+#     # cloud, trial 69
+#     branch_hidden_layers: int = 1
+#     trunk_hidden_layers: int = 9
+#     hidden_size: int = 225
+#     output_factor: int = 63
+#     learning_rate: float = 4e-5  # optimal for ~4000 epochs
+#     activation: nn.Module = nn.Tanh()
+
+
+# @dataclass
+# class LatentNeuralODEConfig:
+#     """Model config for LatentNeuralODE for the simple_ode dataset"""
+
+#     # cloud, trial 63
+#     latent_features: int = 6
+#     coder_layers: int = 2
+#     coder_width: int = 103
+#     learning_rate: float = 3e-4
+#     ode_layers: int = 4
+#     ode_width: int = 197
+#     ode_tanh_reg: bool = True
+#     activation: nn.Module = nn.SiLU()
+#     model_version: str = "v2"
+
+
+# @dataclass
+# class FullyConnectedConfig:
+#     """Model config for FullyConnected for the simple_ode dataset"""
+
+#     # cloud, trial 44
+#     hidden_size: int = 261
+#     num_hidden_layers: int = 1
+#     learning_rate: float = 1e-4
+#     activation: nn.Module = nn.LeakyReLU()
+
+
+# @dataclass
+# class LatentPolyConfig:
+#     """Model config for LatentPoly for the simple_ode dataset"""
+
+#     # cloud, trial 92
+#     latent_features: int = 9
+#     degree: int = 5
+#     learning_rate: float = 3e-4
+#     coder_layers: int = 1
+#     coder_width: int = 86
+#     activation: nn.Module = nn.Mish()
